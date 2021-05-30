933e5c7e6805d6d9db470005249e55af3b66319d
CHRISTOPHE GROLLIER <christophe.grollier@ext.mpsa.com>
2021-05-30 22:07:18 +0200

    added oracle cloud data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
-  apiRoot: "/api/v1"
+  apiRoot: "/api/v1",
+  clientId: "dependency-manager",
+  clientSecret: "6e86bc96-844f-4ff4-8ea0-64ede37849c3",
+  tokenUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/token",
+  authUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/authorize",
+  logoutUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/userlogout",
+  redirectUri: "https://132.145.60.143"
 };
