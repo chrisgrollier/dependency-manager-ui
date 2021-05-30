528dbac592e24625e07ba6abbbc8b91796b4caa3
CHRISTOPHE GROLLIER <christophe.grollier@ext.mpsa.com>
2021-05-30 22:09:59 +0200

    fixed client_id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiRoot: "/api/v1",
-  clientId: "dependency-manager",
-  clientSecret: "6e86bc96-844f-4ff4-8ea0-64ede37849c3",
+  clientId: "319345c7567c486090abdbd26741917c",
+  clientSecret: "466c3e42-3f8e-4f98-87b5-bc76a187395f",
   tokenUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/token",
   authUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/authorize",
   logoutUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/userlogout",
