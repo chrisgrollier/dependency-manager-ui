c5b281d9e25c1010e03abfaa2efaf784da506b87
CHRISTOPHE GROLLIER <christophe.grollier@ext.mpsa.com>
2021-06-07 19:39:16 +0200

    basic scopes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
   authUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/authorize",
   logoutUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/userlogout",
   redirectUri: "https://132.145.60.143",
-  scope: "openid%20urn:opc:idm:t.user.me",
+  scope: "openid%20https%3A%2F%2F132.145.60.143%2Fread%20https%3A%2F%2F132.145.60.143%2Fwrite",
   tokenAuthMode: "header"
 };
