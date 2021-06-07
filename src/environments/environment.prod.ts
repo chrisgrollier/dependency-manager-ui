b6eab58d1cf57a0030307892b442d1ca40fbc1e8
CHRISTOPHE GROLLIER <christophe.grollier@ext.mpsa.com>
2021-06-07 16:47:50 +0200

    Test with get_groups

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
   authUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/authorize",
   logoutUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/userlogout",
   redirectUri: "https://132.145.60.143",
-  scope: "openid%20groups",
+  scope: "openid%20get_groups",
   tokenAuthMode: "header"
 };
