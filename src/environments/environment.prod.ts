8f83ec2c4cdf0e8f978fa7516bf4b5a7c8900046
CHRISTOPHE GROLLIER <christophe.grollier@ext.mpsa.com>
2021-06-07 23:14:05 +0200

    Changed clientId

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   apiRoot: "/api/v1",
-  clientId: "319345c7567c486090abdbd26741917c",
-  clientSecret: "466c3e42-3f8e-4f98-87b5-bc76a187395f",
+  clientId: "75568ed7513242edbc8d0d2f324127d7",
+  clientSecret: "914663c0-8e02-40f1-a18c-27e01bef3e5e",
   tokenUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/token",
   authUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/authorize",
   logoutUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/userlogout",
   redirectUri: "https://132.145.60.143",
-  scope: "openid%20https%3A%2F%2F132.145.60.143%2Fread%20https%3A%2F%2F132.145.60.143%2Fwrite",
+  //scope: "openid%20https%3A%2F%2F132.145.60.143%2Fread%20https%3A%2F%2F132.145.60.143%2Fwrite",
+  scope: "openid",
   tokenAuthMode: "header"
 };
