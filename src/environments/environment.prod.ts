b4e1ab645f33eb3d8df4f75450eeebe790797804
CHRISTOPHE GROLLIER <christophe.grollier@ext.mpsa.com>
2021-06-07 16:23:49 +0200

    added groups in oracle token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
   authUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/authorize",
   logoutUri: "https://idcs-ff72d424ef424056bfc689d6a41302cb.identity.oraclecloud.com:443/oauth2/v1/userlogout",
   redirectUri: "https://132.145.60.143",
-  scope: "openid",
+  scope: "openid%20groups",
   tokenAuthMode: "header"
 };
